0a4118e66e3aa240f9ffbb16f3ca10fdadbf0538
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2024-01-05 21:00:04 +0100

    update

---
+++ filament-settings-crawler/filament_settings_crawler.ps1
@@ -0,0 +1,110 @@
+# Pfade definieren
+$folderPath = "C:\Users\erikw\Documents\GitHub\3d-printing\filament"
+$outputFilePath = "C:\Users\erikw\Documents\GitHub\3d-printing\filament\readme.md"
+
+# Lese die Translatedateien ein
+$Datei1 = Get-Content -Path "C:\Users\erikw\Documents\GitHub\scriptvault\filament-settings-crawler\fdmextruder.def.json.po" -Encoding UTF8
+$Datei2 = Get-Content -Path "C:\Users\erikw\Documents\GitHub\scriptvault\filament-settings-crawler\fdmprinter.def.json.po" -Encoding UTF8
+
+function SucheUndSpeichereZeilennummern {
+    param (
+        [string]$Suchwort
+    )
+
+    $Zeilennummer1 = 1
+    $Zeilennummer2 = 1
+    $Gefunden1 = $false
+    $Gefunden2 = $false
+
+    $Pattern = "msgctxt `"$Suchwort label`""
+
+    foreach ($Zeile in $Datei1) {
+        if ($Zeile -match $Pattern) {
+            $Gefunden1 = $true
+            $gefundeneZeile = $Datei1[$Zeilennummer1 + 1]
+            $gefundeneZeile = $gefundeneZeile -replace 'msgstr "', '' -replace '"', ''
+
+            return $gefundeneZeile
+            # Write-Host "$Suchwort, Inhalt: $gefundeneZeile"
+            break
+        }
+        $Zeilennummer1++
+    }
+
+    foreach ($Zeile in $Datei2) {
+        if ($Zeile -match $Pattern) {
+            $Gefunden2 = $true
+            $gefundeneZeile = $Datei2[$Zeilennummer2 + 1]
+            $gefundeneZeile = $gefundeneZeile -replace 'msgstr "', '' -replace '"', ''
+            # Write-Host "$Suchwort, Inhalt: $gefundeneZeile"
+            return $gefundeneZeile
+            break
+        }
+        $Zeilennummer2++
+    }
+
+    if (-not $Gefunden1 -and -not $Gefunden2) {
+        Write-Host "Nicht in beiden Dateien gefunden"
+    }
+}
+
+$translate = @{}
+
+foreach($key in $propertyValues.Keys) {
+    $translation = SucheUndSpeichereZeilennummern -Suchwort $key
+    $translate[$key] = $translation
+}
+
+
+
+# Suche nach .curaprofile-Dateien im Ordner
+$files = Get-ChildItem -Path $folderPath -Filter *.curaprofile
+
+# Erstelle oder leere die readme.md
+Set-Content -Path $outputFilePath -Value ""
+
+# Erstelle ein Array für die Werte
+$propertyValues = @{}
+
+# Iteriere durch jede .curaprofile-Datei
+foreach ($file in $files) {
+    # Lese den Inhalt der Datei
+    $content = Get-Content -Path $file.FullName
+
+    # Extrahiere den Profilnamen (nur den ersten Treffer)
+    $name = ($content | Select-String -Pattern "name = (.*)" | Select-Object -First 1 | ForEach-Object { $_.Matches.Groups[1].Value }).Trim()
+
+    # Starte das Schreiben in readme.md mit der Überschrift
+    Add-Content -Path $outputFilePath -Value "## $name`n"
+
+    # Starte das Extrahieren der Werte
+    $writing = $false
+    foreach ($line in $content) {
+        if ($line -match "^\[values\]") {
+            $writing = $true
+            continue
+        }
+
+        if ($writing -and [string]::IsNullOrWhiteSpace($line)) {
+            $writing = $false
+            continue  # Entferne die leeren Zeilen
+        }
+
+        if ($writing) {
+            # Extrahiere und speichere die Werte
+            if ($line -match '^\s*(\w+)\s*=\s*(.+)') {
+                $propertyName = $matches[1].Trim()
+                
+                if (-not $propertyValues.ContainsKey($propertyName)) {
+                    $propertyValues[$propertyName] = ""
+                }
+            }
+            # Schreibe die Zeilen in die readme.md
+            Add-Content -Path $outputFilePath -Value $line.Trim()
+        }
+    }
+
+    # Füge eine leere Zeile nach jedem Profil hinzu
+    Add-Content -Path $outputFilePath -Value "`n"
+}
+
