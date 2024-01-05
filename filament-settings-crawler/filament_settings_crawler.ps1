ddb47eb3e23d940a6f841094ecf6e75e20c00802
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2024-01-05 21:05:09 +0100

    Update filament_settings_crawler.ps1

--- filament-settings-crawler/filament_settings_crawler.ps1
+++ filament-settings-crawler/filament_settings_crawler.ps1
@@ -1,6 +1,6 @@
 # Pfade definieren
 $folderPath = "C:\Users\erikw\Documents\GitHub\3d-printing\filament"
-$outputFilePath = "C:\Users\erikw\Documents\GitHub\3d-printing\filament\readme.md"
+$outputFilePath = "C:\Users\erikw\Documents\GitHub\3d-printing\README.md"
 
 # Lese die Translatedateien ein
 $Datei1 = Get-Content -Path "C:\Users\erikw\Documents\GitHub\scriptvault\filament-settings-crawler\fdmextruder.def.json.po" -Encoding UTF8
@@ -48,20 +48,20 @@ function SucheUndSpeichereZeilennummern {
     }
 }
 
-$translate = @{}
-
-foreach($key in $propertyValues.Keys) {
-    $translation = SucheUndSpeichereZeilennummern -Suchwort $key
-    $translate[$key] = $translation
-}
-
-
-
 # Suche nach .curaprofile-Dateien im Ordner
 $files = Get-ChildItem -Path $folderPath -Filter *.curaprofile
 
 # Erstelle oder leere die readme.md
 Set-Content -Path $outputFilePath -Value ""
+
+# Erhalte den aktuellen Zeitstempel
+$currentDateTime = Get-Date
+
+# Füge den Überschrit der Datei hinzu
+Add-Content -Path $outputFilePath -Value "# Filament Settings"
+
+# Füge den Zeitstempel der Datei hinzu
+Add-Content -Path $outputFilePath -Value "> Diese Datei wurde zuletzt am $currentDateTime automatisch generiert."
 
 # Erstelle ein Array für die Werte
 $propertyValues = @{}
@@ -94,13 +94,25 @@ foreach ($file in $files) {
             # Extrahiere und speichere die Werte
             if ($line -match '^\s*(\w+)\s*=\s*(.+)') {
                 $propertyName = $matches[1].Trim()
-                
+                $translation = SucheUndSpeichereZeilennummern -Suchwort $propertyName
+
                 if (-not $propertyValues.ContainsKey($propertyName)) {
                     $propertyValues[$propertyName] = ""
                 }
             }
+            $translate = $line -replace [regex]::Escape($propertyName), $translation
+
+            # Ersetze "True" durch "Ja"
+            $translate  = $translate  -replace '\bTrue\b', 'Ja'
+
+            # Ersetze "False" durch "Nein"
+            $translate = $translate -replace '\bFalse\b', 'Nein'
+
+            # Ersetze "None" durch "Keine"
+            $translate  = $translate  -replace '\bNone\b', 'Keine'
+
             # Schreibe die Zeilen in die readme.md
-            Add-Content -Path $outputFilePath -Value $line.Trim()
+            Add-Content -Path $outputFilePath -Value $translate.Trim() -Encoding UTF8
         }
     }
 
