933b59113217e88fd87b1da856063eef7eb208dd
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2024-01-26 23:30:07 +0100

    Update filament_settings_crawler.ps1

--- filament-settings-crawler/filament_settings_crawler.ps1
+++ filament-settings-crawler/filament_settings_crawler.ps1
@@ -1,48 +1,52 @@
-# Pfade definieren
+# Definiere Pfade für Input- und Output-Dateien
 $folderPath = "C:\Users\erikw\Documents\GitHub\3d-printing\filament"
 $outputFilePath = "C:\Users\erikw\Documents\GitHub\3d-printing\README.md"
 
-# Lese die Translatedateien ein
+# Lese die Inhalte der Dateien ein
 $Datei1 = Get-Content -Path "C:\Users\erikw\Documents\GitHub\scriptvault\filament-settings-crawler\fdmextruder.def.json.po" -Encoding UTF8
 $Datei2 = Get-Content -Path "C:\Users\erikw\Documents\GitHub\scriptvault\filament-settings-crawler\fdmprinter.def.json.po" -Encoding UTF8
 
+# Funktion, um Zeilennummern zu suchen und zu speichern
 function SucheUndSpeichereZeilennummern {
     param (
         [string]$Suchwort
     )
 
+    # Initialisierung von Variablen
     $Zeilennummer1 = 1
     $Zeilennummer2 = 1
     $Gefunden1 = $false
     $Gefunden2 = $false
 
+    # Muster für die Suche
     $Pattern = "msgctxt `"$Suchwort label`""
 
+    # Suche in Datei 1
     foreach ($Zeile in $Datei1) {
         if ($Zeile -match $Pattern) {
             $Gefunden1 = $true
+            # Extrahiere die gefundene Zeile
             $gefundeneZeile = $Datei1[$Zeilennummer1 + 1]
             $gefundeneZeile = $gefundeneZeile -replace 'msgstr "', '' -replace '"', ''
-
             return $gefundeneZeile
-            # Write-Host "$Suchwort, Inhalt: $gefundeneZeile"
             break
         }
         $Zeilennummer1++
     }
 
+    # Suche in Datei 2
     foreach ($Zeile in $Datei2) {
         if ($Zeile -match $Pattern) {
             $Gefunden2 = $true
             $gefundeneZeile = $Datei2[$Zeilennummer2 + 1]
             $gefundeneZeile = $gefundeneZeile -replace 'msgstr "', '' -replace '"', ''
-            # Write-Host "$Suchwort, Inhalt: $gefundeneZeile"
             return $gefundeneZeile
             break
         }
         $Zeilennummer2++
     }
 
+    # Wenn nichts gefunden wurde
     if (-not $Gefunden1 -and -not $Gefunden2) {
         Write-Host "Nicht in beiden Dateien gefunden"
     }
@@ -56,11 +60,12 @@ Set-Content -Path $outputFilePath -Value ""
 
 # Erhalte den aktuellen Zeitstempel
 $currentDateTime = Get-Date
+$currentDateTime = $currentDateTime.ToString("dd.MM.yyyy HH:mm:ss")
 
-# Füge den Überschrit der Datei hinzu
+# Füge den Titel zur Datei hinzu
 Add-Content -Path $outputFilePath -Value "# Filament Settings"
 
-# Füge den Zeitstempel der Datei hinzu
+# Füge den Zeitstempel zur Datei hinzu
 Add-Content -Path $outputFilePath -Value "> Diese Datei wurde zuletzt am $currentDateTime automatisch generiert."
 
 # Erstelle ein Array für die Werte
@@ -74,7 +79,7 @@ foreach ($file in $files) {
     # Extrahiere den Profilnamen (nur den ersten Treffer)
     $name = ($content | Select-String -Pattern "name = (.*)" | Select-Object -First 1 | ForEach-Object { $_.Matches.Groups[1].Value }).Trim()
 
-    # Starte das Schreiben in readme.md mit der Überschrift
+    # Schreibe den Profilnamen in readme.md
     Add-Content -Path $outputFilePath -Value "## $name`n"
 
     # Starte das Extrahieren der Werte
@@ -100,23 +105,17 @@ foreach ($file in $files) {
                     $propertyValues[$propertyName] = ""
                 }
             }
+            # Führe Übersetzungen durch
             $translate = $line -replace [regex]::Escape($propertyName), $translation
-
-            # Ersetze "True" durch "Ja"
             $translate  = $translate  -replace '\bTrue\b', 'Ja'
-
-            # Ersetze "False" durch "Nein"
             $translate = $translate -replace '\bFalse\b', 'Nein'
-
-            # Ersetze "None" durch "Keine"
             $translate  = $translate  -replace '\bNone\b', 'Keine'
 
-            # Schreibe die Zeilen in die readme.md
-            Add-Content -Path $outputFilePath -Value $translate.Trim() -Encoding UTF8
+            $entry = "- $($translate.Trim())"
+            Add-Content -Path $outputFilePath -Value $entry -Encoding UTF8
         }
     }
 
     # Füge eine leere Zeile nach jedem Profil hinzu
     Add-Content -Path $outputFilePath -Value "`n"
 }
-
