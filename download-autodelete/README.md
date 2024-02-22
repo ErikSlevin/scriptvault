b13cdfa2319bcea9c3429cb6219211b9ced7a287
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2024-02-22 18:16:22 +0100

    add Autoclean Download script

---
+++ download-autodelete/README.md
@@ -0,0 +1,11 @@
+# Download Ordner automatisch löschen
+
+Benötigte Abhängigkeiten:
+``` Install-Module -Name PSFolderSize ```
+
+1. ```Download Autoclean.xml``` anpassen ([user] durch eigenen ersetzen)
+2. Aufgabenplanung starten
+3. Eigene -> Aufgabe importieren -> ```Download Autoclean.xml``` 
+4. ```download-autodelete.ps1``` unter ```C:\Users\[user]\skripte\``` abspeichern
+5. Log-Ordner erstellen ```C:\Users\[user]\skripte\logs```
+6. Spaß haben
---
+++ download-autodelete/download-autodelete.ps1
@@ -0,0 +1,26 @@
+﻿Start-Transcript -Path "C:\Users\[USER]\skripte\logs\$(Get-Date -Format "yyyy-MM-dd-HH-mm")_download-autodelete.log"
+try{
+    $Pfad = "C:\Users\[USER]\Downloads\"
+    $CSV = "C:\Users\[USER]\skripte\logs\download-autodelete.csv"
+    $Datum = Get-Date -Format "dd.MM.yyyy"
+    $Uhrzeit = "$(Get-Date -Format "HH:mm") Uhr"
+    if (Test-Path -Path $Pfad){
+        
+        $Inhalt = Get-FolderSize -BasePath $Pfad | Select-Object @{ l='Datum'; e={ $Datum} },@{ l='Uhrzeit'; e={ $Uhrzeit} },@{l = "Datei"; e={$_.FolderName}},@{l = "Dateigröße"; e={$_.SizeMB}}
+        if (Test-Path $CSV -PathType leaf){
+            $Inhalt | Export-CSV -Append $CSV -Encoding UTF8 -Delimiter ';'
+        } else {
+            $Inhalt | Export-CSV $CSV -Encoding UTF8 -Delimiter ';'
+        }
+        
+        Remove-Item $Pfad* -Recurse -Force
+    } 
+}
+catch{
+    Write-Host "$($Datum) | $($Uhrzeit) | $($_.Exception.Message)"
+    
+}
+finally{
+    Write-Host "$($Datum) | $($Uhrzeit) | Download Autoclean erfolgreich ausgeführt!"
+}
+Stop-Transcript
