eee5bd982e10c8e5609d9c82a29bd3c5a997e3fc
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2025-03-04 21:37:31 +0100

    fix

--- download-autodelete/download-autodelete.ps1
+++ download-autodelete/download-autodelete.ps1
@@ -1,26 +1,10 @@
-﻿Start-Transcript -Path "C:\Users\[USER]\skripte\logs\$(Get-Date -Format "yyyy-MM-dd-HH-mm")_download-autodelete.log"
-try{
-    $Pfad = "C:\Users\[USER]\Downloads\"
-    $CSV = "C:\Users\[USER]\skripte\logs\download-autodelete.csv"
-    $Datum = Get-Date -Format "dd.MM.yyyy"
-    $Uhrzeit = "$(Get-Date -Format "HH:mm") Uhr"
-    if (Test-Path -Path $Pfad){
-        
-        $Inhalt = Get-FolderSize -BasePath $Pfad | Select-Object @{ l='Datum'; e={ $Datum} },@{ l='Uhrzeit'; e={ $Uhrzeit} },@{l = "Datei"; e={$_.FolderName}},@{l = "Dateigröße"; e={$_.SizeMB}}
-        if (Test-Path $CSV -PathType leaf){
-            $Inhalt | Export-CSV -Append $CSV -Encoding UTF8 -Delimiter ';'
-        } else {
-            $Inhalt | Export-CSV $CSV -Encoding UTF8 -Delimiter ';'
-        }
-        
-        Remove-Item $Pfad* -Recurse -Force
-    } 
+﻿# Definiere den Pfad zum Downloads-Ordner des aktuellen Benutzers
+$Pfad = "$env:USERPROFILE\Downloads\"
+
+try {
+    # Versuche, alle Dateien und Unterordner im Downloads-Ordner zu löschen
+    Remove-Item "$Pfad*" -Recurse -Force  # Lösche alle Inhalte im definierten Pfad
+    Write-Output "Alle Dateien und Ordner im Downloads-Ordner wurden erfolgreich gelöscht."
+} catch {
+    Write-Error "Fehler beim Löschen der Dateien und Ordner im Downloads-Ordner: $_"
 }
-catch{
-    Write-Host "$($Datum) | $($Uhrzeit) | $($_.Exception.Message)"
-    
-}
-finally{
-    Write-Host "$($Datum) | $($Uhrzeit) | Download Autoclean erfolgreich ausgeführt!"
-}
-Stop-Transcript
